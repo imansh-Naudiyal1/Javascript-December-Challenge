44f6bfcc0e5ccb55565594a33544573adf3b51e3
Himanshu Naudiyal <harishnaudiyal2@gmail.com>
2025-11-14 14:12:10 +0530

    Day 1

---
+++ Day- 01/index.js
@@ -0,0 +1,18 @@
+// Day - 01: JavaScript Basics
+
+// Topic Covered:
+//  Variables (let, const)
+//  Storing user information
+//  String output using console.log()
+//  Basic understanding of data types
+
+
+let firstName = "Himanshu";
+let lastName = "Naudiyal";
+let age = 23;
+let isStudent = "I am a student";
+const fatherName = " harish Naudiyal";
+console.log("My Name is:", firstName,lastName ,"I am:",age ,"years old:", isStudent);
+
+
+
---
+++ Day-02/script.js
@@ -0,0 +1,15 @@
+// Day 2: Conditions (if / else)
+
+// Topic Covered:
+// if / else statements
+//  Modulus operator (%)
+//  Checking conditions
+//  Printing different outputs based on logic
+
+let number = 2;
+
+if(number % 2 === 0){
+    console.log(number, "is an Even Number");
+}else{
+    console.log(number, "is an Odd Number");
+}
