0910f603143fdbd68cb85d1368ad9d65905ebf81
Himanshu Naudiyal <harishnaudiyal2@gmail.com>
2025-11-20 13:46:43 +0530

    loops

---
+++ Day-03/loops.js
@@ -0,0 +1,43 @@
+// Program: Sum of all even numbers from 1 to 50
+
+// let sum = 0; 
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 === 0) {
+//     sum += i; 
+//   }
+// }
+
+// console.log("Sum of even numbers from 1 to 50:", sum);
+
+// Task1
+
+// for (let i = 0 ; i <= 20; i++){
+// console.log(i);
+// }
+
+//Task2
+
+// let number = 0;
+ 
+// for( let i = 0; i <= 20; i++ ) {
+//    if ( i % 2 === 0 ) {
+//       number += i;
+//    }
+
+// }
+// console.log("Sum of even numbers from 0 to 20 is: " + number);
+
+// Task3
+
+//  for (let  i= 10; i >= 1; i--){
+//     console.log(i);
+//  }
+
+// Task4
+
+let table = 0;
+for ( let i = 1; i <= 10; i++ ) {
+   table = 5 * i;
+   console.log("5 x " + i + " = " + table);
+}
